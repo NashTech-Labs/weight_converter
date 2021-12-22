5db574e36314d708e1b10d51c6169328fb445548
ankit-mogha <ankit.mogha@knoldus.com>
2021-12-22 15:49:54 +0530

    Add comments for functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use std::io;
+
 mod converter;
 mod test;
 
-fn main() {
-    converter::weight_converter::convert_weight();
+fn main()  {
+    println!("Please enter your weight ");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+                converter::weight_converter::convert_weight(input);
+            },
+        Err(error) => {
+            println!("{}", error.to_string());
+        }
+    }
 }
 
 
--- src/test.rs
+++ src/test.rs
@@ -1,9 +1,14 @@
 #[cfg(test)]
 mod test {
-    use crate::converter::weight_converter::calculate_weight_on_mars;
+    use crate::converter::weight_converter::{calculate_weight_on_mars, convert_weight};
 
     #[test]
-    fn check_calculate_weight_on_mars_success() {
+    fn check_calculate_weight_on_mars() {
         assert_eq!(calculate_weight_on_mars(100.0), 37.828747);
+    }
+
+    #[test]
+    fn check_convert_weight() {
+        assert_eq!(convert_weight("100.0".to_string()),"100.0".to_string());
     }
 }
